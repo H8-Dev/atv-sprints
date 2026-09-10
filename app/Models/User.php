3e0660629dce760853274fe1a1d8b28501b8c73d
H8-Dev <h8.pr0j1@gmail.com>
2026-09-10 11:04:32 -0300

    Sprint 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    
+    public function perguntas(): HasMany
+    {
+        return $this->hasMany(Pergunta::class);
+    }
 }
